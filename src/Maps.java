3b659b3eaf9bb92d231321807e11096893c396a2
Xenek-source <darkpik@gmail.com>
2022-12-29 12:45:27 +0100

    :)

--- src/Maps.java
+++ src/Maps.java
@@ -3,16 +3,9 @@ public class Maps {
     Scanner scan = new Scanner(System.in);
     public Maps[][] plansza = new Maps[3][3];
     public Maps pole;
-    public String user1, user2;
     public Maps pion1, pion2, pion3, pion4, pion5, pion6;
-
-
 public Maps(){
 }
-public void userAdd(String a,String b){
-        user1 = a;
-        user2 = b;
-    }
 public void dodajPionki1(){
     pion1 = new Maps();
     plansza[0][0] = pion1;
@@ -29,9 +22,6 @@ public void dodajPionki2(){
     pion6 = new Maps();
     plansza[2][2] = pion6;
     }
-/*public void getPion(){
-    System.out.print(plansza[0][0]);
-}*/
 public void drawMaps(){
     System.out.println();
     System.out.print("                   ");
